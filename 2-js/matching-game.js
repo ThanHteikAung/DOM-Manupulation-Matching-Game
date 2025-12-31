053b682237921779c4765370a4ff5a5b2625564d
Than Hteik Aung <thanhtaikaung90@gmail.com>
2025-12-31 17:53:35 +0630

    Finish coding ( enjoy play game) Finding different between left and right side -show game over when click any where -go to the next level when click correct face ( not include rightside of face). Finding not including face in leftside.

--- 2-js/matching-game.js
+++ 2-js/matching-game.js
@@ -1,8 +1,8 @@
 let numberOfFaces = 5;
 const theLeftSide = document.querySelector("#leftSide");
+const theRightSide = document.querySelector('#rightSide');
 
 function generateFaces() {
-    alert("IN" + numberOfFaces)
     let face;
     let randomTop;
     let randomLeft;
@@ -17,4 +17,31 @@ function generateFaces() {
         face.style.left = randomLeft + 'px';
         theLeftSide.appendChild(face);
     }
+    const leftSideImages = theLeftSide.cloneNode(true);
+    leftSideImages.removeChild(leftSideImages.lastChild);
+    theRightSide.appendChild(leftSideImages);
+
+    theLeftSide.lastChild.addEventListener('click', nextLevel);
+    document.body.addEventListener('click', gameOver);
+
 }
+
+function nextLevel(event) {
+    event.stopPropagation();
+    numberOfFaces += 5;
+    while (theLeftSide.children.length > 0) {
+        theLeftSide.removeChild(theLeftSide.firstChild)
+    }
+    while (theRightSide.children.length > 0) {
+        theRightSide.removeChild(theRightSide.firstChild)
+    }
+    generateFaces();
+}
+
+function gameOver() {
+    alert("Game Over");
+    document.body.removeEventListener('click', gameOver);
+    theLeftSide.lastChild.removeEventListener('click', nextLevel);
+
+}
+
